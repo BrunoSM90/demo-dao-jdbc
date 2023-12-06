bd32b81467eafdbaddfc216ca6b47813f22ca37e
Bruno Salgado Mazzola <smbrunosm@gmail.com>
2023-12-05 22:43:05 -0300

    Department class fix (int id to integer id)

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -6,14 +6,14 @@ import java.util.Objects;
 public class Department implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private int id;
+	private Integer id;
 	private String name;
 	
 	public Department() {
 
 	}
 
-	public Department(int id, String name) {
+	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
@@ -22,7 +22,7 @@ public class Department implements Serializable {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -53,6 +53,6 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		return id == other.id && Objects.equals(name, other.name);
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
 	}
 }
